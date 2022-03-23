f20fa9b5b63c5c57c103580be6e3c3372c3459d9
Twesh14 <85089662+Twesh14@users.noreply.github.com>
2022-03-23 21:28:20 +0530

    Add files via upload

---
+++ Motion Sensor Blynk Notification.ino
@@ -0,0 +1,38 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "14ABi4l8ASdjn7AvoNU8yLaDql2hZB67";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "My";
+char pass[] = "12345678";
+
+#define inPin 5
+int count = 0; 
+
+void setup()
+{
+  
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(inPin,INPUT);
+  Blynk.notify("Mission Started");
+}
+
+void loop()
+{
+  Blynk.run();
+  bool value = digitalRead(inPin);
+  if (value == 1) { 
+    count++;
+    Blynk.notify("Value is true");
+    Blynk.virtualWrite(V1,count);
+  }
+  delay(3000);
+}
